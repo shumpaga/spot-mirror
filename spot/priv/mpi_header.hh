fd7c5d33ceea1cc00991d99673304529000ce6d5
Alexis Di Lisi <adilisi@antananarivo.lrde.epita.fr>
2018-07-13 14:31:27 +0200

    spot: fixing Open-MPI support
    * configure.ac,
    spot/priv/mpi_header.hh: here.

--- spot/priv/mpi_header.hh
+++ spot/priv/mpi_header.hh
@@ -17,6 +17,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+/* trick to avoid Werrors when including "mpi.h" */
+
 #pragma once
 #pragma GCC system_header
 
